a430e1c99512a86f3ecd3ef634b734a1a8f49c91
Acea Spades <aceaspades@protonmail.com>
2022-12-14 17:38:25 +0200

    -Added favicons and og-image -Added Meta component

---
+++ lib/utils/AppConfig.ts
@@ -0,0 +1,6 @@
+export const AppConfig = {
+    site_name: 'AceaSpades-NextJS-Template',
+    title: 'AceaSpades-NextJS-Template',
+    description: 'AceaSpades-NextJS-Template',
+    locale: 'en',
+};
