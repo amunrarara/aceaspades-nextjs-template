ff4a3df3822abf7eeb87d5efdcfa8ade5de24d3c
Acea Spades <aceaspades@protonmail.com>
2023-01-30 23:41:36 +0200

    Added font-size and line-height custom properties to globals.css, then set tailwind.config.json to use these values; fixed package.json error

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,19 @@ module.exports = {
     extend: {
       fontSize: {
         'navbar-height': 'var(--navbar-height)',
+        xs: ['var(--fs-xs)', { lineHeight: 'var(--lh-xs)' }],
+        sm: ['var(--fs-sm)', { lineHeight: 'var(--lh-sm)' }],
+        base: ['var(--fs-base)', { lineHeight: 'var(--lh-base)' }],
+        lg: ['var(--fs-lg)', { lineHeight: 'var(--lh-lg)' }],
+        xl: ['var(--fs-xl)', { lineHeight: 'var(--lh-xl)' }],
+        '2xl': ['var(--fs-2xl)', { lineHeight: 'var(--lh-2xl)' }],
+        '3xl': ['var(--fs-3xl)', { lineHeight: 'var(--lh-3xl)' }],
+        '4xl': ['var(--fs-4xl)', { lineHeight: 'var(--lh-4xl)' }],
+        '5xl': ['var(--fs-5xl)', { lineHeight: 'var(--lh-5xl)' }],
+        '6xl': ['var(--fs-6xl)', { lineHeight: 'var(--lh-5xl)' }],
+        '7xl': ['var(--fs-7xl)', { lineHeight: 'var(--lh-5xl)' }],
+        '8xl': ['var(--fs-8xl)', { lineHeight: 'var(--lh-5xl)' }],
+        '9xl': ['var(--fs-9xl)', { lineHeight: 'var(--lh-5xl)' }],
       },
       animation: {
         'pulse-slow': 'pulse 3s ease-out infinite',
